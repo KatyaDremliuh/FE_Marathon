80571da3df42eeb60a60de2e41aa3646215bbd85
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-08-31 22:49:07 +0300

    Add drad and drop cards

---
+++ 02_drag-n-drop/app.js
@@ -0,0 +1,43 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover); // перетаскиваемый эл-т нах. над местом, куда можно его поместить
+    placeholder.addEventListener('dragenter', dragenter); // когда заходим на территорию плейсхолдера
+    placeholder.addEventListener('dragleave', dragleave); // когда перетащили, но вышли оттуда
+    placeholder.addEventListener('drop', dragdrop); // когда отпустили
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold');
+    setTimeout(() => event.target.classList.add('hide'), 0);
+}
+
+function dragend(event) {
+    // event.target.classList.remove('hold', 'hide');
+    event.target.className = 'item';
+}
+
+function dragover(event) {
+    // console.log('drag over')
+    event.preventDefault();
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered');
+    // console.log('drag enter')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered');
+    // console.log('drag leave')
+}
+
+function dragdrop(event) {
+    // console.log('drag drop')
+    event.target.classList.remove('hovered');
+    event.target.append(item);
+}
