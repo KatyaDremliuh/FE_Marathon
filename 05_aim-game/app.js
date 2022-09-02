35afeef1fe0b7884781c4746ee1b12ab141b034a
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-09-02 21:37:25 +0300

    Add circle

--- 05_aim-game/app.js
+++ 05_aim-game/app.js
@@ -1,7 +1,9 @@
 const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
-let time = 0;
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+let time = 12;
 
 startBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -10,6 +12,58 @@ startBtn.addEventListener('click', (event) => {
 
 timeList.addEventListener('click', event => {
     if (event.target.classList.contains('time-btn')) {
-        console.log(event.target);
+        time = parseInt(event.target.getAttribute('data-time'));
+
+        screens[1].classList.add('up');
+
+        startGame();
     }
 })
+
+// DEBUG
+startGame();
+
+function startGame() {
+    createRandomCircle();
+
+    setInterval(decreaseTime, 1000);
+
+    setTime(time);
+}
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame();
+    } else {
+        let current = --time;
+
+        if (current < 10) {
+            current = `0${current}`;
+        }
+        setTime(current);
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame(params) {
+
+}
+
+function createRandomCircle(params) {
+    const circle = document.createElement('div');
+
+    const size = getRandomNumber(10, 60);
+
+    circle.classList.add('circle');
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+
+    board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
